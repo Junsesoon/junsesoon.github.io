40ffaa48780d2164ef4b8b1c3ea33b006c335fa5
junsesoon <junsesoon@gmail.com>
2026-05-13 21:14:56 +0900

    Install React + Next

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Next.js 엔진 이식 완료🚀</h1>
+      <p>기존 Vanilla JS 코드와 마크다운 파일들을 이곳으로 이식 하면 됨</p>
+    </main>
+  )
+}
