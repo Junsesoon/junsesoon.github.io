0d0eb9d9a370a6592d500ed68a69b86806661f66
junsesoon <junsesoon@gmail.com>
2026-05-25 22:43:47 +0900

    Hotfix: apply Google Analytics 4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import GNB from '@/components/GNB';
 import '@/styles/globals.css';
 import '@/styles/atom-one-dark.css';
+import Script from 'next/script';
 
 export const metadata = {
   title: 'Junseo Blog',
@@ -15,6 +16,26 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className="m-0 p-0 flex flex-col min-h-screen bg-white text-gray-900">
+        {process.env.GA_MEASUREMENT_ID && (
+          <>
+            <Script
+              strategy="afterInteractive"
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
+            />
+            <Script
+              id="google-analytics"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${process.env.GA_MEASUREMENT_ID}');
+                `,
+              }}
+            />
+          </>
+        )}
         <GNB />
         <div className="pt-16 flex-1">
           {children}
